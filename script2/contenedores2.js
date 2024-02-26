d036b475a8972c9ebe10ba2a4dac6bc2fcf466c4
Juan Roman <161282135+Juanlink24@users.noreply.github.com>
2024-02-25 22:36:02 -0800

    Add files via upload

--- script2/contenedores2.js
+++ script2/contenedores2.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body")
 
-const colors = ["blue", "cyan", "pink", "red", "orange", "yellow", "gray", "green", "purple", "brown"]
+const colors = ["silver", "gray", "maroon", "red", "purple", "fuchsia", "green", "lime", "olive", "yellow",
+ "blue", "teal", "aqua", "beige", "brown", "chocolate", "crimson", "salmon", "gold", "greenyellow"]
 
 for(let i = 0; i < 10; i++) {
     const div = document.createElement('div')
